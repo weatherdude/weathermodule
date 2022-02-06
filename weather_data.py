b6274b471fbdaf36402670a7f3b25e973e426679
weatherdude <j.heusinger@tu-bs.de>
2022-02-06 13:43:20 +0100

    added current weather function

--- weather_data.py
+++ weather_data.py
@@ -9,7 +9,8 @@ start_date = '2022-01-22T15:00:00'     # date format yyyy-MM-dd or yyyy-MM-ddTHH
 end_date = '2022-01-22T15:00:00'
 
 location = 'Braunschweig'
-
+lat = 52.276187
+lon = 10.538474
 unitGroup = 'metric' # Supported values are us, uk, metric, base - default US
 
 
@@ -22,6 +23,40 @@ data = data.json()
 
 # forecast
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Braunschweig?key='+api_key+'&unitGroup='+unitGroup+'&include=alerts'
+
+# current conditions
+url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+location+'?key='+api_key+'&unitGroup='+unitGroup+'&include=current'
+
+def current_weather(lat,lon,unitGroup):
+    # lat: latitude (°)
+    # lon: longitude (°)
+
+    latlon = str(lat)+','+str(lon)
+    url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+latlon+'?key='+api_key+'&unitGroup='+unitGroup+'&include=current'
+    data = requests.get(url)
+    data = data.json()
+    data_df = pd.DataFrame.from_dict(data['currentConditions']).iloc[0]
+    temperature = data_df['temp']               # US = Fahrenheit; Metric = Celsius
+    relative_humidity = data_df['humidity']     # (%)
+    dew_point = data_df['dew']                  # US = Fahrenheit; Metric = Celsius
+    precipitation = data_df['precip']           # US = Inches ; Metric = Millimeters
+    snow = data_df['snow']                      # US = Inches ; Metric = Centimeters
+    windgust = data_df['windgust']              # US = MpH ; Metric = KpH
+    windspeed = data_df['windspeed']            # US = MpH ; Metric = KpH
+    winddir = data_df['winddir']                # (°)
+    pressure = data_df['pressure']              # Mb/hPa
+    visibility = data_df['visibility']          # US = Miles ; Metric = KM
+    cloudcover = data_df['cloudcover']          # (%)
+    solarradiation = data_df['solarradiation']  # (W m-2)
+    uvindex = data_df['uvindex']
+    conditions = data_df['conditions']
+    station_ID = data_df['stations']
+    sunrise = data_df['sunrise']
+    sunset = data_df['sunset']
+    moonphase = data_df['moonphase']            # 0 = new moon ; 0.5 = full moon
+
+    return temperature,relative_humidity,dew_point,precipitation,snow,windgust,windspeed,winddir,pressure,visibility,cloudcover,solarradiation,uvindex,conditions,station_ID,sunrise,sunset,moonphase
+
 
 def weather_at_obs(start_date,end_date,lat,lon,unitGroup):
     # Get historical weather data for specific date and time
